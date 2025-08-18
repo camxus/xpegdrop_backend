6b2e3d6d74b2a1425800c2ed4bad53194e5a8687
camxus <camillus.konkwo@gmail.com>
2025-08-18 18:31:42 +0200

    update cors

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,15 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://xpegdrop.eba-enjhhiwz.eu-west-1.elasticbeanstalk.com",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
