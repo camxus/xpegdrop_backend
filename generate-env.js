34f720a5bb3bf1bd8f16afb4f6c76ece030c5026
camxus <camillus.konkwo@gmail.com>
2025-08-20 18:14:56 +0200

    disallow keys

--- generate-env.js
+++ generate-env.js
@@ -2,12 +2,20 @@ const fs = require("fs");
 const path = require("path");
 require("dotenv").config(); // load local .env if present
 
+const reservedKeys = [
+  "AWS_REGION",
+  "AWS_DEFAULT_REGION",
+  "AWS_ACCESS_KEY_ID",
+  "AWS_SECRET_ACCESS_KEY",
+];
+
+
 // Regex pattern: only keep env vars starting with these prefixes
 const allowedPattern = /^(AWS_|SERVERLESS_|EXPRESS_|PUBLIC_)/;
 
 // Collect env vars dynamically
 const envVars = Object.keys(process.env).filter(
-  (key) => process.env[key] !== undefined && allowedPattern.test(key)
+  (key) => process.env[key] !== undefined && allowedPattern.test(key) && !reservedKeys.includes(key)
 );
 
 // Convert to .env format
