965d11c3db52c54b72095cca749afbe1a00d9c93
camxus <camillus.konkwo@gmail.com>
2025-08-20 13:27:09 +0200

    add approved_emails

--- src/utils/validation/projectValidation.ts
+++ src/utils/validation/projectValidation.ts
@@ -4,6 +4,7 @@ export const createProjectSchema = Joi.object({
   name: Joi.string().min(1).max(100).required(),
   description: Joi.string().max(500).optional(),
   is_public: Joi.boolean().optional(),
+  approved_emails: Joi.array().optional(),
   file_locations: Joi.string().optional()
 });
 
@@ -11,5 +12,6 @@ export const updateProjectSchema = Joi.object({
   name: Joi.string().min(1).max(100).optional(),
   description: Joi.string().max(500).optional(),
   is_public: Joi.boolean().optional(),
+  approved_emails: Joi.array().optional(),
   file_locations: Joi.string().optional()
 });
