ba4aa098d0fd18b463e047b067fb9202b46136da
camxus <camillus.konkwo@gmail.com>
2025-08-20 11:33:56 +0200

    revert cors

--- src/server.ts
+++ src/server.ts
@@ -21,8 +21,6 @@ app.use(
     origin: [
       process.env.FRONTEND_URL || "",
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "x-id-token"],
     credentials: true,
   })
 );
